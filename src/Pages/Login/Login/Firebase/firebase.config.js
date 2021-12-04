2f8b8e7f1d048d75b142d57622dfa6eaccc63c14
mukitur <mukit.inway@gmail.com>
2021-12-05 01:32:35 +0600

    authentication done

--- src/Pages/Login/Login/Firebase/firebase.config.js
+++ src/Pages/Login/Login/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.envREACT_APP_APP_ID,
   };
 
 
@@ -15,5 +15,7 @@ const firebaseConfig = {
   storageBucket: "creative-agency-bf566.appspot.com",
   messagingSenderId: "572529997411",
   appId: "1:572529997411:web:4c6ab4f5e779229c846a56"
+
+   
 }; */
 export default firebaseConfig;
