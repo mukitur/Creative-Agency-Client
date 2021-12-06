d290a3953eba9a011ee43f6f5a7238c2f0f62d62
mukitur <mukit.inway@gmail.com>
2021-12-06 18:51:45 +0600

    order page created

--- src/Pages/AllOrder/OrderNow/OrderNow.js
+++ src/Pages/AllOrder/OrderNow/OrderNow.js
@@ -1,12 +1,44 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 import Navigation from '../../../Shared/Navigation/Navigation';
+import { useForm } from "react-hook-form";
+import axios from 'axios';
+import './OrderNow.css';
 
 const OrderNow = () => {
+    const { register, handleSubmit, reset } = useForm();
+    const onSubmit = data => {
+        console.log(data);
+        axios.post('http://localhost:8000/orders', data)
+        .then(res=>{
+            console.log(res);
+            if(res.data.insertedId){
+                alert('added successfully');
+            }
+            reset();
+        })
+    }   
+
     return (
-        <div>
-            <Navigation/>
-            <h2>Order Now</h2>
-        </div>
+        <>
+             <Navigation/>
+            <div className="add-orders">
+                <Typography style={{textAlign: 'center'}} variant="h3" className="my-5">
+                    Order Page
+                </Typography>
+                <Typography style={{textAlign: 'center'}} variant="body2" className="my-5">
+                    Thanks for choosing Creative Agency!
+                </Typography>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input  {...register("name", { required: true, maxLength: 80 })} placeholder="Name"/>
+                    <input  {...register("email", { required: true, maxLength: 30 })} placeholder="Email"/>
+                    <input  {...register("cell", { required: true, maxLength: 30 })} placeholder="Cell Number"/>
+                    <textarea {...register("address", { required: true, maxLength: 800 })} placeholder="Shipping Address" />
+                    
+                    <input type="submit"/>
+                </form>
+            </div>
+        </>
     );
 };
 
